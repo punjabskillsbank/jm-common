ed118445ad55d8b8dc28d828b2c081dc3c3f0952
pawanpreet193 <pawanpreetsingh193@gmail.com>
2025-04-04 19:05:29 +0530

    JMK_44: added enums AccountStatus and UserRole for user entity

---
+++ src/main/java/com/common/enums/AccountStatus.java
@@ -0,0 +1,11 @@
+package com.common.enums;
+
+public enum AccountStatus {
+    ACTIVE,
+    DEACTIVATED,
+    DELETED,
+    SUSPENDED,
+    BANNED,
+    PENDING,
+    TO_BE_DELETED
+}
---
+++ src/main/java/com/common/enums/UserRole.java
@@ -0,0 +1,8 @@
+package com.common.enums;
+
+public enum UserRole {
+    CLIENT,
+    FREELANCER,
+    ADMIN,
+    CLIENT_FREELANCER
+}
