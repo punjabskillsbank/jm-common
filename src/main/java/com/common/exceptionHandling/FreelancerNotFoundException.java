3449d5844ec268472f32bad1e6da7fdb150cfbe8
sahibsingh13254 <sahibs98765432@gmail.com>
2025-05-19 19:18:39 +0530

    JMK_32: Added the FreelancerNotFoundException

---
+++ src/main/java/com/common/exceptionHandling/FreelancerNotFoundException.java
@@ -0,0 +1,10 @@
+package com.common.exceptionHandling;
+
+import java.util.UUID;
+
+
+public class FreelancerNotFoundException extends RuntimeException {
+    public FreelancerNotFoundException(UUID freelancerId) {
+        super("Freelancer not found with ID: " + freelancerId);
+    }
+}
